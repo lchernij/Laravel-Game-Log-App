34c0493768ac2bd155208649aa57bd3c8259e597
Leandro Chernij <lchernij@gmail.com>
2024-12-11 16:47:55 -0300

    Teste de feature do 2FA

--- tests/Feature/Admin/LoginTest.php
+++ tests/Feature/Admin/LoginTest.php
@@ -10,14 +10,15 @@ class LoginTest extends TestCase
 {
     #[Group('Orchid')]
     #[Group('3-adicionar-testes-de-api-para-o-orchid')]
+    #[Group('17-adicionar-two-factor-authentication-2fa')]
     public function testLogin(): void
     {
         # Show login form
-        $this->get('/admin/login')
+        $this->get('/login')
             ->assertStatus(200);
 
         # Form validations
-        $this->post('/admin/login', [
+        $this->post('/login', [
             'email' => '',
             'password' => ''
         ])
@@ -27,13 +28,13 @@ class LoginTest extends TestCase
                 'password' => 'O campo senha é obrigatório.'
             ]);
 
-        $this->post('/admin/login', [
+        $this->post('/login', [
             'email' => '123',
             'password' => '123'
         ])
             ->assertStatus(302)
             ->assertSessionHasErrors([
-                'email' => 'Os dados que você inseriu não correspondem aos nossos registros. Verifique e tente novamente.'
+                'email' => 'Essas credenciais não foram encontradas em nossos registros.'
             ]);
 
         # User can login with success
@@ -42,11 +43,19 @@ class LoginTest extends TestCase
             'password' => bcrypt('123123')
         ]);
 
-        $this->post('/admin/login', [
+        $this->post('/login', [
             'email' => 'admin@admin.com',
             'password' => '123123'
         ])
             ->assertStatus(302)
             ->assertRedirect('/admin/main');
+
+        # User two factory challenge too many requests
+        for ($i = 0; $i < 5; $i++) {
+            $this->post('/two-factor-challenge', []);
+        }
+
+        $this->post('/two-factor-challenge', [])
+            ->assertStatus(429);
     }
 }
